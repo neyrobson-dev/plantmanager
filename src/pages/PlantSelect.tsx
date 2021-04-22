85998e3e3096128af78ba4e1c571a8c0aac6646d
Ney Robson <ney.robson@kbank.com.br>
2021-04-22 17:56:39 -0300

    first commit

---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import { Button } from '../components/Button';
+
+export function PlantSelect() {
+    return (
+        <SafeAreaView>
+            <Text>Selecione planta</Text>
+        </SafeAreaView>
+    );
+}
