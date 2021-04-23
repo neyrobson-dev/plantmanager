f148d0634ddae255ea32de65ff4a2e572d3bb922
Ney Robson <ney.robson@kbank.com.br>
2021-04-22 21:49:47 -0300

    .

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+import Constants from 'expo-constants';
+
+import profileImg from '../assets/profile.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá</Text>
+                <Text style={styles.userName}>Ney Robson</Text>
+            </View>
+
+            <Image source={profileImg} style={styles.image} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: Constants.statusBarHeight,
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text,
+    },
+    userName: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 40
+    },
+    image: {
+        width: 60,
+        height: 60,
+        borderRadius: 30
+    }
+});
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,17 +1,132 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     SafeAreaView,
     StyleSheet,
     Text,
-    View
+    View,
+    FlatList
 } from 'react-native';
 
+import { Header } from '../components/Header';
+import { EnvironmentButton } from '../components/EnvironmentButton';
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
 import { Button } from '../components/Button';
 
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import api from '../services/api';
+
+interface EnvironmentProps {
+    key: string;
+    title: string;
+}
+
+interface PlantProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: string;
+    frequency: string;
+    times: string;
+    repeat_every: string;
+}
+
 export function PlantSelect() {
+    const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+    const [plants, setPlants] = useState<PlantProps[]>([]);
+
+    useEffect(() => {
+        async function fetchEnvironmen() {
+            const { data } = await api.get('plants_environments');
+            setEnvironments([
+                {
+                    key: 'all',
+                    title: 'Todos'
+                },
+                ...data
+            ]);
+        }
+
+        fetchEnvironmen();
+    }, []);
+
     return (
-        <SafeAreaView>
-            <Text>Selecione planta</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Header />
+                <Text style={styles.title}>Em qual ambiente</Text>
+                <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+            </View>
+
+            <View>            
+                <FlatList 
+                    data={environments}
+                    renderItem={({ item }) => (
+                        <EnvironmentButton
+                            key={item.key}
+                            title={item.title}
+                        />
+                    )}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.environmentList}
+                />
+            </View>
+
+            <View style={styles.plants}>
+                <FlatList 
+                    data={environments}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary
+                            key={item.key}
+                            data={[
+                                {
+                                    name: 'teste',
+                                    photo: '',
+                                }                                
+                            ]}
+                        />
+                    )}
+                    showsHorizontalScrollIndicator={false}
+                />
+            </View>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.background,
+    },
+    header: {
+        paddingHorizontal: 30
+    },
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.text,
+        lineHeight: 20,
+    },
+    environmentList: {
+        height: 40,
+        justifyContent: 'center',
+        paddingBottom: 5,
+        marginHorizontal: 32,
+        marginVertical: 32
+    },
+    plants: {
+        flex: 1,
+        paddingHorizontal: 32,
+        justifyContent: 'center'
+    }
+});
