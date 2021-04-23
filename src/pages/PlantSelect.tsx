96929abd5eb7ca60b495147b02dc245b2ed81e78
Ney Robson <ney.robson@kbank.com.br>
2021-04-23 10:36:28 -0300

    Load de pagina e load de lista

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -4,13 +4,14 @@ import {
     StyleSheet,
     Text,
     View,
-    FlatList
+    FlatList,
+    ActivityIndicator
 } from 'react-native';
 
 import { Header } from '../components/Header';
 import { EnvironmentButton } from '../components/EnvironmentButton';
 import { PlantCardPrimary } from '../components/PlantCardPrimary';
-import { Button } from '../components/Button';
+import { Load } from '../components/Load';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
@@ -27,19 +28,27 @@ interface PlantProps {
     about: string;
     water_tips: string;
     photo: string;
-    environments: string;
-    frequency: string;
-    times: string;
-    repeat_every: string;
+    environments: [string];
+    frequency: {
+        times: number;
+        repeat_every: string;
+    }
 }
 
 export function PlantSelect() {
     const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
     const [plants, setPlants] = useState<PlantProps[]>([]);
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+    const [environmentSelected, setEnvironmentSelected] = useState('all');
+    const [loading, setLoading] = useState(true);
+
+    const [page, setPage] = useState(1);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [loadedAll, setLoadedAll] = useState(false);
 
     useEffect(() => {
-        async function fetchEnvironmen() {
-            const { data } = await api.get('plants_environments');
+        async function fetchEnvironments() {
+            const { data } = await api.get('plants_environments?_sort=title&order=asc');
             setEnvironments([
                 {
                     key: 'all',
@@ -49,8 +58,52 @@ export function PlantSelect() {
             ]);
         }
 
-        fetchEnvironmen();
+        fetchEnvironments();
     }, []);
+
+    useEffect(() => {
+        fetchPlants();
+    }, []);
+
+    async function fetchPlants() {
+        const { data } = await api.get(`plants?_sort=name&order=asc&_page=${page}&_limit=8`);
+        if(!data)
+            return setLoading(true);
+
+        if (page > 1) {
+            setPlants(oldValue => [...oldValue, ...data]);
+            setFilteredPlants(oldValue => [...oldValue, ...data]);
+        } else {
+            setPlants(data);
+            setFilteredPlants(data);
+        }
+
+        setLoading(false);
+        setLoadingMore(false);
+    }
+
+    function handleEnvironmentSelected(environment: string) {
+        setEnvironmentSelected(environment);
+
+        if (environment === 'all')
+            return setFilteredPlants(plants);
+
+        const filtered = plants.filter(plant => plant.environments.includes(environment));
+
+        setFilteredPlants(filtered);
+    }
+
+    function handleFetchMore(distance: number) {
+        if (distance < 1)
+            return;
+
+        setLoadingMore(true);
+        setPage(oldValue => oldValue + 1);
+        fetchPlants();
+    }
+
+    if (loading)
+        return <Load />;
 
     return (
         <View style={styles.container}>
@@ -65,8 +118,9 @@ export function PlantSelect() {
                     data={environments}
                     renderItem={({ item }) => (
                         <EnvironmentButton
-                            key={item.key}
                             title={item.title}
+                            active={item.key == environmentSelected}
+                            onPress={() => handleEnvironmentSelected(item.key)}
                         />
                     )}
                     horizontal
@@ -77,19 +131,19 @@ export function PlantSelect() {
 
             <View style={styles.plants}>
                 <FlatList 
-                    data={environments}
+                    data={filteredPlants}
                     renderItem={({ item }) => (
-                        <PlantCardPrimary
-                            key={item.key}
-                            data={[
-                                {
-                                    name: 'teste',
-                                    photo: '',
-                                }                                
-                            ]}
-                        />
+                        <PlantCardPrimary data={item} />
                     )}
-                    showsHorizontalScrollIndicator={false}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
+                    onEndReachedThreshold={0.1}
+                    onEndReached={({ distanceFromEnd }) => handleFetchMore(distanceFromEnd)}
+                    ListFooterComponent={
+                        loadingMore
+                        ? <ActivityIndicator color={colors.green} />
+                        : <></>
+                    }
                 />
             </View>
         </View>
@@ -121,12 +175,11 @@ const styles = StyleSheet.create({
         height: 40,
         justifyContent: 'center',
         paddingBottom: 5,
-        marginHorizontal: 32,
-        marginVertical: 32
+        margin: 32,
     },
     plants: {
         flex: 1,
         paddingHorizontal: 32,
         justifyContent: 'center'
-    }
+    },
 });
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.70:3333',
+    baseURL: 'http://192.168.1.96:3333',
+    //'http://192.168.1.70:3333',
 });
 
 export default api;
